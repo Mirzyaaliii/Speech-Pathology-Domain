92ee45610f4d5892b6529c2aed73850c4a2afe1c
Mirali Purohit <46327378+Mirzyaaliii@users.noreply.github.com>
2019-09-08 17:17:31 +0530

    Delete sox_sample_channel.sh

--- Improve Intelligibility of Dysarthric Speech/Scripts/sox_sample_channel.sh
+++
@@ -1,9 +0,0 @@
-
-#/bin/sh
-for entry in `ls /media/mihir/Dysarthia/Mirali/TORGO/New_F/* | sort -V `;do
-fname=`basename $entry .wav`
-echo $fname
-sox -r 16000 -b 16 -c 1 $entry /media/mihir/Dysarthia/Mirali/TORGO/New_F/train/$fname.wav 
-done
-exit
-
